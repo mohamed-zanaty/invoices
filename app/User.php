b76fe1b30d629d5b361920ce022b52f2cf786aea
mohamed zanaty <mohamedzanatyyyyy@gmail.com>
2020-12-28 01:59:39 +0200

    first commit

---
+++ app/User.php
@@ -0,0 +1,29 @@
+<?php
+namespace App;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+class User extends Authenticatable
+{
+use Notifiable;
+use HasRoles;
+/**
+* The attributes that are mass assignable.
+*
+* @var array
+*/
+protected $fillable = [
+'name', 'email', 'password','roles_name','Status'
+];
+
+protected $hidden = [
+'password', 'remember_token',
+];
+
+protected $casts = [
+'email_verified_at' => 'datetime',
+'roles_name' => 'array',
+
+];
+}
